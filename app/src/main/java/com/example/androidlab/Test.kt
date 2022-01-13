c890486f12b91b147968e06a02305fd63067f771
daniel <danielkang09@naver.com>
2022-01-13 12:35:52 +0900

    test git

---
+++ app/src/main/java/com/example/androidlab/Test.kt
@@ -0,0 +1,13 @@
+package com.example.androidlab
+
+fun main() {
+    var data1: Int = 10
+    var data2: String? = null
+
+    data1 = data1 + 10
+    data2 = "doo"
+    data1 = data1.plus(10)
+
+    println(data1)
+    println(data2)
+}
