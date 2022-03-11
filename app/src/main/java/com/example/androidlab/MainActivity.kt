3e5f350a5cb8efd8060a69151b25d700318f858d
daniel <danielkang09@naver.com>
2022-03-11 20:48:21 +0900

    test git

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.androidlab
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //이름 문자열 출력 TextView 생성
+        val name = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise"
+        }
+
+        // 이미지 출력 ImageView 생성
+        val image = ImageView(this).apply {
+            it.setImageDrawalbe(ContextCompat.getDrawable(this, R.drawalbe.lake_1))
+        }
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/androidlab/Test.kt
+++ app/src/main/java/com/example/androidlab/Test.kt
@@ -1,13 +1,9 @@
 package com.example.androidlab
 
+
 fun main() {
-    var data1: Int = 10
-    var data2: String? = null
-
-    data1 = data1 + 10
-    data2 = "doo"
-    data1 = data1.plus(10)
-
-    println(data1)
-    println(data2)
+    var a = 1
+    println(a++)
+    println(a)
 }
+
